9f271dc6ad7405505d16612548731342f09b532e
Riddle Trick <riddlev9@gmail.com>
2014-12-17 18:48:32 +0100

    se añade una nueva llinea al cuerpo principal

--- HolaMundo/src/Home.java
+++ HolaMundo/src/Home.java
@@ -3,6 +3,6 @@ public class Home {
 	 public static void main(String[] args) {
 		System.out.println("Hola Mundo");
 		
-		System.out.println("se añade esta linea al proyecto");
+		System.out.println("se añade esta linea al proyecto.");
 	}
 }
